abc56aa3a4d5518e6e8cbe91621cdf7a6a3c5285
ntorilarry <lntori99@gmail.com>
2025-10-02 11:48:21 +0000

    fix bugs 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Dashboard from "./dashboard/page";
+import { useAuth } from "@/utils/authContext";
+import Loader from "@/components/loader";
 
 const Home = () => {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
+  const router = useRouter();
+  const { token, loading } = useAuth();
+
+  useEffect(() => {
+    if (!loading && !token) {
+      router.push("/auth/login");
+    }
+  }, [loading, token, router]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!token) {
+    return null;
+  }
+
+  return <Dashboard />;
 };
 
 export default Home;
